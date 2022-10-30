3611e1ee20441310899df28e44189cafe0f2fd65
Marcelo918 <111820089+Marcelo918@users.noreply.github.com>
2022-10-30 13:29:09 -0700

    I got the program to work. Now I am going to look for bugs.

--- recursion/src/main/java/com/recursion/App.java
+++ recursion/src/main/java/com/recursion/App.java
@@ -1,4 +1,5 @@
 package com.recursion;
+import java.util.Scanner;
 
 /**
  * Hello world!
@@ -8,6 +9,34 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        boolean isRunning = true;
+
+        System.out.print("Please enter the base number: ");
+
+        Scanner userInput = new Scanner(System.in);
+        float base = userInput.nextFloat();
+
+        System.out.print("Please enter a positive power number: ");
+        int n = userInput.nextInt();
+
+        while(n <= 0) {
+
+            System.out.print("Please enter a positive power number: ");
+            n  = userInput.nextInt();
+        }
+
+        float result = power(base, n);
+
+        System.out.println(base + "^" + n + "=" + result);
+        userInput.close();
+    }
+
+    public static float power(float base, int n) {
+        if (n != 0) {
+            return (base * power(base, n - 1));
+        }
+        else {
+            return 1;
+        }
     }
 }
