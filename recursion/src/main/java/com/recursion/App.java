91af362889fc22515fd7f9a417e95e2dc4ffd4c0
Marcelo918 <111820089+Marcelo918@users.noreply.github.com>
2022-11-04 10:24:40 -0700

    Fixed some bugs. Now the code works better.

--- recursion/src/main/java/com/recursion/App.java
+++ recursion/src/main/java/com/recursion/App.java
@@ -1,41 +1,47 @@
 package com.recursion;
+
 import java.util.Scanner;
 
 /**
- * Hello world!
- *
+ * Author: Marcelo Villalobos Diaz
+ * Date: November 04, 2022
+ * Class: CSIS26 - FA22
+ * Description: Program that computes a recursive function x^n.
+ * x should be a floating number, n should be an integer and it cannot be negative.
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        boolean isRunning = true;
-
+public class App {
+    public static void main(String[] args) {
         System.out.print("Please enter the base number: ");
 
+        // scans for the user to input a floating number
         Scanner userInput = new Scanner(System.in);
         float base = userInput.nextFloat();
 
+        // scans again but for a positive whole number now
         System.out.print("Please enter a positive power number: ");
         int n = userInput.nextInt();
 
-        while(n <= 0) {
+        // if n is less than 0 then it will prompt
+        // the user to input a new value again
+        while (n < 0) {
 
             System.out.print("Please enter a positive power number: ");
-            n  = userInput.nextInt();
+            n = userInput.nextInt();
         }
 
         float result = power(base, n);
 
-        System.out.println(base + "^" + n + "=" + result);
+        // prints the result
+        System.out.print(base + " ^ " + n + " = ");
+        System.out.printf("%.4f%n", result);
         userInput.close();
     }
 
+    // recursive function
     public static float power(float base, int n) {
         if (n != 0) {
             return (base * power(base, n - 1));
-        }
-        else {
+        } else {
             return 1;
         }
     }
